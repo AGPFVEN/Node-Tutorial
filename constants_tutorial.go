b50702379ce47202b628684f562be284f7f27130
AGPFVEN <alfonso_pineda2003@hotmail.com>
2022-08-19 00:20:22 +0200

    constants in go

---
+++ constants_tutorial.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	//"math"
+)
+
+//iota constants
+const (
+	u = iota
+	i										//This syntax is the same as:
+	o2										//o2 = iota
+)
+
+func main() {
+	//iota 
+	fmt.Printf("%v, %T\n", u, u)
+	fmt.Printf("%v, %T\n", i, i)
+	fmt.Printf("%v, %T\n", o2, o2)
+
+	//Obviously a constant cannot be reassigned
+	//A constant can be shadowed
+	const u int = 5
+	fmt.Printf("%v, %T\n", u, u)
+
+	const q int = 1							//Typed constant, as a typed variable
+	fmt.Printf("%v, %T\n", q, q)
+
+	const r = 16							//Constants can non-strictly typed
+	var t int16 = 27
+	var y int64 = 27
+	fmt.Printf("%v, %T\n", r + t, r + t)	//Constants can be implicitly converted
+	fmt.Printf("%v, %T\n", r + y, r + y)
+	//fmt.Printf("%v, %T\n", t + y, t + y)	//Variables cannot
+
+	//const w = math.sin(0.9)	A constant cannot be assigned in runtime, it must be declared and assigned at the moment it is created
+}
