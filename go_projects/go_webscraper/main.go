609ef94b5f2f699a3d44bd1573eb1023e0f5ea32
AGPFVEN <alfonso_pineda2003@hotmail.com>
2022-08-25 22:35:57 +0200

    Not understanding very well chromedp

--- go_projects/go_webscraper/main.go
+++ go_projects/go_webscraper/main.go
@@ -2,29 +2,39 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
+	"github.com/chromedp/cdproto/cdp"
 	"github.com/chromedp/chromedp"
 )
 
-func main() {
-	//This is the code to actually see the browser
-	//ctx, cancel := chromedp.NewExecAllocator(context.Background(), append(chromedp.DefaultExecAllocatorOptions[:], chromedp.Flag("headless", false))...)
-	//defer cancel()
-
-	ctx, cancel := chromedp.NewContext(context.Background())
+func main(){
+	//Initializing Browser Context (if headless mode is not disabled this doesn't work)
+	ctx, cancel := chromedp.NewExecAllocator(
+		context.Background(),
+		append(chromedp.DefaultExecAllocatorOptions[:], chromedp.Flag("headless", false))...
+	)
 	defer cancel()
 
-	var res string
+	ctx, cancel = chromedp.NewContext(ctx)
+	defer cancel()
 
-	//The Query options may be the key
-	err := chromedp.Run(ctx, 
-		chromedp.Navigate("https://web.whatsapp.com/"),
+	//var data string
+	var data []*cdp.Node
+	var data_status bool
+
+	log.Println("Initializing Browser...")
+
+	err := chromedp.Run(ctx,
+		chromedp.Navigate("http://web.whatsapp.com/"),
 		chromedp.WaitEnabled("._25pwu", chromedp.ByQuery),
-		chromedp.OuterHTML("html", &res, chromedp.ByQuery),
-	)
+		//chromedp.InnerHTML("._2UwZ_", &data, chromedp.ByQuery),
+		chromedp.Nodes("._2UwZ_", &data),
+		)
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println(res)
+	fmt.Printf(string(data))
+	log.Println(data_status)
 }
