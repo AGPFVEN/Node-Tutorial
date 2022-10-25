91c719a2993ba8836deecbf4b8dfe0d907400ffd
AGPFVEN <alfonso_pineda2003@hotmail.com>
2022-10-25 23:25:25 +0200

    Revert "Get XML and parse it"
    This reverts commit 3043efdb19468b6e967acc1a300123f421f88cbd.

--- go_fundamentals/practical_go/web.go
+++ go_fundamentals/practical_go/web.go
@@ -6,14 +6,13 @@ import (
 )
 
 func index_handler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w, "<h1>Whoa, Go is neat!</h1>")
 	fmt.Fprint(w, "Whoa, Go is neat!")
 }
 func about_handler(w http.ResponseWriter, r *http.Request){
 	fmt.Fprint(w, "Not the first time creating a function that is used as a router")
 }
 
-func web(){
+func web_(){
 	http.HandleFunc("/", index_handler)
 	http.HandleFunc("/about/", about_handler)
 	http.ListenAndServe(":8000", nil)
