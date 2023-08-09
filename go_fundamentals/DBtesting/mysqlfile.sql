d49a8f2b758b03f150c708c7b36c70df78ceba1a
Alfonso Pineda <agpfven@gmail.com>
2023-08-09 02:05:44 +0200

    this is the problem with uploading a file to the database

--- go_fundamentals/DBtesting/mysqlfile.sql
+++ go_fundamentals/DBtesting/mysqlfile.sql
@@ -3,6 +3,18 @@ use testDB;
 DROP TABLE IF EXISTS testDB1;
 
 CREATE TABLE testDB1 (
-    pnumber int(9) NOT NULL,
+    pnumber CHAR(9) NOT NULL,
     pzip BLOB
 );
+
+INSERT INTO testDB1(pnumber, pzip) VALUE ("000000001", LOAD_FILE("/DBtesting/testzip.zip"));
+
+/*
+mysql> show variables like 'secure_file_priv'
+    -> ;
++------------------+-----------------------+
+| Variable_name    | Value                 |
++------------------+-----------------------+
+| secure_file_priv | /var/lib/mysql-files/ |
++------------------+-----------------------+
+1 row in set (0,00 sec)
