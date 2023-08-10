e74eb282f9c37631ad51a94ac590cdb0df26b15b
Alfonso Pineda <agpfven@gmail.com>
2023-08-10 18:57:52 +0200

    testing mysql with go

--- go_fundamentals/DBtesting/dbs.go
+++ go_fundamentals/DBtesting/dbs.go
@@ -1,3 +1,24 @@
 package main
 
-//testDB is the database I am using (use testDB)
+//user: ellicn8kupq98dtk3jk7
+//password: pscale_pw_9s98hoDsemF1hsQsrrM0vkAmGIHmoAHreSY4go3qTCR
+//.env DSN=ellicn8kupq98dtk3jk7:pscale_pw_9s98hoDsemF1hsQsrrM0vkAmGIHmoAHreSY4go3qTCR@tcp(aws.connect.psdb.cloud)/wss_test?tls=true
+
+import (
+    "database/sql"
+    "log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+    db, err := sql.Open("mysql", "ellicn8kupq98dtk3jk7:pscale_pw_9s98hoDsemF1hsQsrrM0vkAmGIHmoAHreSY4go3qTCR@tcp(aws.connect.psdb.cloud)/wss_test?tls=true")
+    if err != nil {
+        log.Fatalf("failed to connect: %v", err)
+    }
+    defer db.Close()
+    if err := db.Ping(); err != nil {
+        log.Fatalf("failed to ping: %v", err)
+    }
+    log.Println("Successfully connected to PlanetScale!")
+}
---
+++ go_fundamentals/go.mod
@@ -0,0 +1,5 @@
+module github.com/agpfven/go_tutorial/go_fundamentals
+
+go 1.20
+
+require github.com/go-sql-driver/mysql v1.7.1 // indirect
